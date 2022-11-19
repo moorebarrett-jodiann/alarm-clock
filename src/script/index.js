e29c98de7f87dc389f8be90c35efae8e555a80fa
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2022-11-18 19:25:45 -0600

    update clock js

--- src/script/index.js
+++ src/script/index.js
@@ -29,9 +29,12 @@ function print(arg) {
 /**--------------------------------------------------------------------------- */
 
 const liveTime = select('.time');
-let userInput = select('.input');
+const startBtn = select('.start');
+const play = new Audio('./src/audio/alarm.mp3');
+play.type = 'audio/mp3';
 let alarmHour;
 let alarmMinute;
+let alarmMessage = select('.message');
 
 setInterval(function() {
   let today = new Date();
@@ -41,14 +44,45 @@ setInterval(function() {
   let currSecond = today.getSeconds();
 
   liveTime.innerHTML = currHour + ":" + currMinute + ":" + currSecond;
-  if (currHour == alarmhour && currMinute == alarmminute) {
-    liveTime.style.color = '#33ab4e';
+  if (currHour == alarmHour && currMinute == alarmMinute) {
+    setTimeout(() => { 
+      liveTime.style.color = '#33ab4e';
+      start.play();
+    }, 10_000);
   }
 
-}, 500)
+}, 500);
+
+// display the alarm the user has set
+function newAlarm(hour, minute) {
+  alarmHour = hour;
+  alarmMinute = minute;
+  let message = `<i class="fa-solid fa-bell"></i> <span>${hour}:</span><span>${minute}</span>`;
+  alarmMessage.innerHTML = message;
+}
+
+// generate the alarm the user has set and clear input
+function addAlarm() {
+  const userInput = select('.user-input');
+  const userInputVal = userInput.value.trim();
+  const userInputArr = userInputVal.split(':');
+  let hour = parseInt(userInputArr[0]);
+  let minute = parseInt(userInputArr[1]);
+  if (hour, minute) {
+    newAlarm(hour, minute);
+    userInput.value = "";
+  }
+};
 
 // format hour to 12-hour format
 function formatHoursTo12(date) {
   return date.getHours() % 12 || 12;
 }
+
+// on button click make alarm target visible
+onEvent('click', startBtn, () => {
+  const userInput = select('.user-input').value.trim();
+  console.log(userInput);
+  addAlarm();
+});
 
